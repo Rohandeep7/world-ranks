18f8291575e165d28eb9d26657134992cb9c153b
Rohandeep7 <ronnddy7@gmail.com>
2024-01-22 01:08:55 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-[#1B1D1F]">
+    <div class="relative">
+      <Image img-class="w-full" :src="HERO_GLOBE_IMAGE" />
+      <Image
+        class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 w-[18%]"
+        :src="WEBSITE_LOGO"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Image from "./components/common/Image.vue";
+import HERO_GLOBE_IMAGE from "./assets/hero-image-wr.jpg";
+import WEBSITE_LOGO from "./assets/Logo.svg";
+</script>
+
+<style scoped></style>
---
+++ src/components/common/Image.vue
@@ -0,0 +1,56 @@
+<template>
+  <Transition name="slide-bottom-fade" appear>
+    <v-lazy-image
+      class="rounded-md"
+      :class="imgClass"
+      :src="src"
+      :src-placeholder="srcPlaceholder"
+      :alt="alt"
+      :width="width"
+      :height="height"
+    />
+  </Transition>
+</template>
+
+<script setup>
+import VLazyImage from "v-lazy-image";
+import { Transition } from "vue";
+
+const props = defineProps({
+  src: {
+    type: String,
+    required: true,
+  },
+  srcPlaceholder: {
+    type: String,
+    required: false,
+  },
+  alt: {
+    type: String,
+    default: "Image",
+  },
+  imgClass: {
+    type: String,
+    required: false,
+  },
+  width: {
+    type: String,
+    required: false,
+  },
+  height: {
+    type: String,
+    required: false,
+  },
+});
+</script>
+
+<style scoped>
+@import "../../transitions.scss";
+.v-lazy-image {
+  filter: blur(4px);
+  transition: filter 0.7s;
+}
+.v-lazy-image-loaded {
+  filter: blur(0);
+}
+</style>
