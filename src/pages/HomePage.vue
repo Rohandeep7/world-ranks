01ac0dcac52c1290b3152df719551fe2392561e5
Rohandeep7 <ronnddy7@gmail.com>
2024-01-24 01:11:04 +0530

    created search and results block outline

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <header class="relative">
+      <Image
+        img-class="w-full h-[28vh] sm:h-[35vh] object-cover"
+        :src="HERO_GLOBE_IMAGE"
+      />
+      <Image
+        class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10"
+        :src="WEBSITE_LOGO"
+      />
+    </header>
+
+    <main class="relative">
+      <div
+        class="absolute sm:top-1/2 sm:left-1/2 w-full sm:w-[90vw] bg-[#1B1D1F] sm:border-2 sm:border-[#282B30] rounded-lg min-h-[50vh] sm:-translate-x-1/2 sm:-translate-y-[15%]"
+      ></div>
+    </main>
+  </div>
+</template>
+
+<script setup>
+import Image from "../components/common/Image.vue";
+import HERO_GLOBE_IMAGE from "../assets/hero-image-wr.jpg";
+import WEBSITE_LOGO from "../assets/Logo.svg";
+</script>
+
+<style lang="scss" scoped></style>
