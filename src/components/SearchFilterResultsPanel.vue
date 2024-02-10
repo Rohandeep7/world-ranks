f695dc0a27b7ddf120c49d657eb128bcc3ac20fd
Rohandeep7 <ronnddy7@gmail.com>
2024-02-10 20:01:11 +0530

    setup basic table design

--- src/components/SearchFilterResultsPanel.vue
+++ src/components/SearchFilterResultsPanel.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="absolute sm:top-1/2 sm:left-1/2 w-full sm:max-w-[90vw] bg-black-pearl sm:border-2 sm:border-[#282B30] rounded-lg sm:-translate-x-1/2 sm:-translate-y-[15%]"
+    class="absolute sm:top-1/2 sm:left-1/2 w-full sm:max-w-[90vw] bg-black-pearl sm:border-2 sm:border-[#282B30] rounded-lg sm:-translate-x-1/2 sm:-translate-y-[5%]"
   >
     <div class="m-6">
       <section class="flex flex-col gap-6">
@@ -12,13 +12,55 @@
           <div class="w-full sm:w-1/4">
             <FilterPanel @on-filter-change="filterChangeHandler" />
           </div>
-          <div class="sm:w-4/5">
-            <!-- <div v-if="isLoading">Loading...</div>
-            <ul class="flex w-full flex-wrap text-white gap-2" v-else>
-              <li v-for="country in listOfCountries">
-                {{ country.name.common }}
-              </li>
-            </ul> -->
+          <div
+            class="sm:w-4/5 max-h-screen text-link-water font-be-vietnam-pro"
+          >
+            <div class="overflow-x-auto shadow-md sm:rounded-lg">
+              <table class="w-full max-h-40">
+                <thead
+                  class="text-xs text-raven border-b-2 border-bunker font-semibold text-left"
+                >
+                  <tr>
+                    <th
+                      scope="col"
+                      class="py-3"
+                      v-for="(field, idx) in RESULT_PANEL_TABLE_FIELDS"
+                      :key="idx"
+                    >
+                      {{ field }}
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr
+                    class=""
+                    v-for="(country, idx) in getAllCountriesEvent.result.value"
+                    :key="idx"
+                  >
+                    <td class="py-4">
+                      <Image
+                        :width="36"
+                        :height="20"
+                        v-if="country.flags"
+                        :src="country.flags.png"
+                      />
+                    </td>
+                    <td class="py-4">
+                      {{ country.name.common }}
+                    </td>
+                    <td class="py-4">
+                      {{ country.population }}
+                    </td>
+                    <td class="py-4">
+                      {{ country.area }}
+                    </td>
+                    <td class="py-4">
+                      {{ country.region }}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </section>
@@ -29,25 +71,23 @@
 <script setup>
 import SearchArea from "./SearchArea.vue";
 import FilterPanel from "./FilterPanel.vue";
+import Image from "./common/Image.vue";
 import countryService from "../services/CountryService";
 import usePromise from "../composables/common/use-promise";
+import { RESULT_PANEL_TABLE_FIELDS } from "../constants/index";
 
 const filterChangeHandler = (filters) => {
   console.log(Array.isArray(filters));
 };
 
-const {
-  result: listOfCountries,
-  isLoading,
-  run: countriesEventRun,
-} = usePromise({
+const getAllCountriesEvent = usePromise({
   target: (params) => countryService.getAllCountries(params),
   onSuccess: (response) => {
     console.log("Countriesss", response);
   },
 });
 
-countriesEventRun();
+getAllCountriesEvent.run();
 </script>
 
 <style lang="scss" scoped></style>
--- src/constants/index.js
+++ src/constants/index.js
@@ -24,6 +24,13 @@ export const REGIONS = [
 
 export const COUNTRY_STATUS = ["Member of the United Nations", "Independent"];
 
+export const RESULT_PANEL_TABLE_FIELDS = [
+  "Flag",
+  "Name",
+  "Population",
+  "Area (km²)",
+  "Region",
+];
 export const BASE_URL = {
   REST_COUNTRIES_API: import.meta.env.VITE_REST_COUNTRIES_API_BASE_URL,
 };
